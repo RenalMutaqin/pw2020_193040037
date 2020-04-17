22493782157824fd662778add02ad71f4e27699a
RenalMutaqin <60698356+RenalMutaqin@users.noreply.github.com>
2020-04-18 02:22:54 +0700

    Update tubes_193040037.sql

--- kuliah/pertemuan9/tubes_193040037.sql
+++ kuliah/pertemuan9/tubes_193040037.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Apr 2020 pada 08.02
+-- Waktu pembuatan: 17 Apr 2020 pada 21.04
 -- Versi server: 10.4.8-MariaDB
 -- Versi PHP: 7.1.33
 
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `makanan` (
-  `id` char(30) NOT NULL,
+  `id` int(11) NOT NULL,
   `Nama_Makanan` char(50) DEFAULT NULL,
   `Asal_Makanan` char(30) DEFAULT NULL,
   `Bahan_Makanan` char(90) DEFAULT NULL,
-  `Jenis_Makanan` char(30) DEFAULT NULL,
+  `Jenis` char(30) DEFAULT NULL,
   `Harga` int(11) DEFAULT NULL,
   `Gambar` char(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -42,17 +42,17 @@ CREATE TABLE `makanan` (
 -- Dumping data untuk tabel `makanan`
 --
 
-INSERT INTO `makanan` (`id`, `Nama_Makanan`, `Asal_Makanan`, `Bahan_Makanan`, `Jenis_Makanan`, `Harga`, `Gambar`) VALUES
-('001', 'Bolu Kukus', 'Indonesia', 'Terigu, Gula Pasir, Susu', 'Kering', 2000, 'Bolu.jpg'),
-('002', ' Kol Goreng', 'Indonesia', 'Kol', 'Kering', 2000, 'kol.jpg'),
-('003', 'Susu', 'Eropa', 'Susu', 'Minuman', 5000, 'susu.jpg'),
-('004', 'Nasi Goreng', 'Indonesia', 'Nasi, Kecap, Telur, Sosis/Baso, Sayuran, Garam', 'Kering', 15000, 'nasi.jpg'),
-('005', 'Sambal', 'Indonesia', 'Tomat, Gula, Cabe, Garam', 'Basah', 2000, 'sambal.jpg'),
-('006', 'Mie Aceh', 'Indonesia (Aceh)', 'Mie dan Bahan Lain', 'Basah & Berkuah', 15000, 'mie.jpg'),
-('007', 'Lontong', 'Indonesia', 'Beras', 'Kering', 1000, 'lontong.jpg'),
-('008', 'Pisang Aroma', 'Indonesia', 'Kulit Lumpia, Pisang, Gula', 'Kering', 1000, 'pisang.jpg'),
-('009', 'Bakwan', 'Indonesia', 'Terigu, Sayuran, Garam', 'Kering', 1000, 'bakwan.jpg'),
-('010', 'Bubur', 'Indonesia', 'Beras, Ayam, Kacang, Kari', 'Basah', 5000, 'bubur.jpg');
+INSERT INTO `makanan` (`id`, `Nama_Makanan`, `Asal_Makanan`, `Bahan_Makanan`, `Jenis`, `Harga`, `Gambar`) VALUES
+(1, 'Es Krim', 'China', 'Susu, Gula, Telur, Vanili', 'Basah', 5000, 'eskrim.jpeg'),
+(2, 'Bolu Kukus', 'Indonesia', 'Terigu, Gula Pasir, Susu', 'Kering', 2000, 'bolu.jpg'),
+(3, 'Kol Goreng', 'Indonesia', 'Kol', 'Kering', 2000, 'kol.jpg'),
+(4, 'Jus Alpukat', 'Meksiko', 'Alpukat, Air, Susu, Gula', 'Minuman', 10000, 'alpukat.jpg'),
+(5, 'Susu', 'Eropa', 'Susu', 'Minuman', 5000, 'susu.jpg'),
+(6, 'Nasi Goreng', 'Indonesia', 'Nasi, Kecap, Sosis, Baso, Ayam, Sayuran, Garam', 'Kering', 13000, 'nasi.jpg'),
+(7, 'Yogurt', 'India', 'Susu, Bakteri Baik', 'Basah', 20, 'Yogurt'),
+(8, 'Sambal', 'Indonesia', 'Cabe, Gula, Tomat, Garam', 'Basah', 2000, 'sambal.jpg'),
+(9, 'Keju', 'Italia', 'Susu', 'Kering', 15, 'keju.jpg'),
+(10, 'Kopi', 'Abbysinia', 'Biji Kopi', 'Minuman', 20000, 'kopi.jpeg');
 
 --
 -- Indexes for dumped tables
@@ -63,6 +63,16 @@ INSERT INTO `makanan` (`id`, `Nama_Makanan`, `Asal_Makanan`, `Bahan_Makanan`, `J
 --
 ALTER TABLE `makanan`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `makanan`
+--
+ALTER TABLE `makanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
